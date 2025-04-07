56c7d37f9f4b273ce047c067e448850c97ec5c7d
ArthurHenrique <agsarth1234@gmail.com>
2025-04-06 21:05:59 -0300

    Update loolp.java

--- loolp.java
+++ loolp.java
@@ -1,7 +1,7 @@
 public class loolp{
     public static void main(String[] args) {
                 
-        for (int i = 20; i >= 0; i--) {
+        for (int i = 20; i >= 0; i++) {
             System.out.println(i); 
         }
         for (int i = 0; i < 21; i++) {
